33c194b528b1cdbeea7e388242fe85d8b8825e6b
youssef.medhat <yossfmedhat@gmail.com>
2023-09-23 17:32:07 +0300

    added docker compose

--- Task2/Task2/settings.py
+++ Task2/Task2/settings.py
@@ -77,11 +77,15 @@ WSGI_APPLICATION = 'Task2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Headway',
+        'USER': 'youssefmedhat',
+        'PASSWORD': '12345678',
+        'HOST': 'db', 
+        'PORT': '',  
+    }
 }
+
 
 
 # Password validation
